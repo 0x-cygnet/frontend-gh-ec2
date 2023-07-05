54dba582591bb2dc2986c56c17b8495d6600a32d
0x-cygnet <bob@cygnetops.com>
2023-07-05 15:44:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React v1
+          Learn React v2
         </a>
       </header>
     </div>
